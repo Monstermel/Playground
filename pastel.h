745cc89ef4755d79d71ed219264eeb3a08b3726c
Monstermel <abreualvarez@hotmail.com>
2020-06-03 19:19:29 -0500

    Se agrego const a argumentos de texto_pastel

--- pastel.h
+++ pastel.h
@@ -4,7 +4,7 @@
 void imprimir_titulo();//Agregar esta funcion o poner titulo al la funcion graficos pastel
 void rellenar_pastel(int pattern,int a, int b, int c);
 void animacion_pastel(int x, int y, int inicio, int final, int radio);
-void texto_pastel(int x, int y, char *imp_1, float porc, char *imp_2, int val, int a, int b, int c);
+void texto_pastel(int x, int y, const char *imp_1, float porc, const char *imp_2, int val, int a, int b, int c);
 void graficos_pastel(long long total, long long a, long long b);//Funcion principal
 
 #endif // PASTEL_H_INCLUDED
