fe3ebfc7706dc716b27f77472be96c2b930a1c12
Monstermel <abreualvarez@hotmail.com>
2020-06-06 18:52:11 -0500

    Se ordenaron los prototipos

--- barras.h
+++ barras.h
@@ -1,9 +1,9 @@
 #ifndef BARRAS_H_INCLUDED
 #define BARRAS_H_INCLUDED
 
-void impresion_lineas(int max, int y_long);
-void graficos_barras(const char *texto, size_t dias, const unsigned int *casos);
 unsigned int altura(unsigned int n, unsigned int max, unsigned int y);
+void impresion_lineas(int max, int y_long);
 void impresion_barras(int prtc_2, int max, int y_long, size_t dias, const unsigned int *casos);
+void graficos_barras(const char *texto, size_t dias, const unsigned int *casos);
 
 #endif // BARRAS_H_INCLUDED
