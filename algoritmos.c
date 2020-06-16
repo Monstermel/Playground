167a3370e7a0f2282688b274e0df98a29bfc1bae
Monstermel <abreualvarez@hotmail.com>
2020-06-15 19:41:30 -0500

    Ordenado del codigo y modificacion de funcion
    Esa funcion se modifico para que haya mas lineas en la cuadricula de las
    graficas de barras

--- algoritmos.c
+++ algoritmos.c
@@ -1,15 +1,80 @@
-#include <math.h>
-#include <stdio.h>
-#include "algoritmos.h"
 
-size_t digit_count(int n){
-    double aux = floor(log10(n));
-    size_t ans = 1 + (size_t)(aux);
-    return ans;
+#include <stdio.h>
+#include <math.h>
+
+#include "algoritmos.h"
+#include "registros.h"
+
+//Insertion sort
+void sort_fechas(Fecha *fechas, size_t elementos){
+    int i, j;
+    Fecha aux;
+    for (i = 1; i < elementos; i++){
+        aux = fechas[i];
+        j = i - 1;
+        while (j >= 0 && comparar_dia(fechas[j], aux)== 1){
+            fechas[j+1] = fechas[j];
+            j --;
+        }
+       fechas[j+1] = aux;
+    }
+    return;
+}
+
+//Regresa si uno si a es mayor o igual que b
+//y 0 si b es mayor a y -1 si son iguales
+int comparar_dia(Fecha a, Fecha b){
+
+    if(a.anio > b.anio){
+        return 1;
+    }
+    else if(a.anio < b.anio){
+        return 0;
+    }
+    else{
+        if(a.mes > b.mes){
+            return 1;
+        }
+        else if(a.mes < b.mes){
+            return 0;
+        }
+        else{
+            if(a.dia > b.dia){
+                return 1;
+            }
+            else if(a.dia < b.dia){
+                return 0;
+            }
+            else{
+                return -1;
+            }
+        }
+    }
+
+}
+
+_uint integer_round(_uint n){
+
+    int potencia = digit_count(n) - 3;
+
+    //Este if es para que numeros como 1 se
+    //redonde a 5 y que 8 se redonde a 10
+	if (potencia < 0){
+		potencia = 0;
+	}
+
+    //Variable de comparacion
+	_uint cmp = 5 * int_pow(10, potencia);
+
+    while (n % cmp != 0){
+        n++;
+    }
+
+    return n;
 }
 
 //Mi implementacion de exponential squaring
-int int_pow(int n, unsigned int m){
+int int_pow(int n, _uint m){
     if(m==0){
         return 1;
     }
@@ -24,22 +89,15 @@ int int_pow(int n, unsigned int m){
     }
 }
 
-unsigned int integer_round(unsigned int n){
-
-    int potencia = digit_count(n) - 2;
-
-	if (potencia < 0){
-		//Este if es para que numeros como 1 se redonde a 5
-		//y que 8 se redonde a 10
-		potencia = 0;
-	}
-
-    //Variable de comparacion
-	unsigned int cmp = 5 * int_pow(10, potencia);
-
-    while (n % cmp != 0){
-        n++;
-    }
-
-    return n;
+size_t digit_count(int n){
+    double aux = floor(log10(n));
+    size_t ans = 1 + (size_t)(aux);
+    return ans;
 }
+
+
+
+
+
+
+
