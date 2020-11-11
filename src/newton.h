ceb59417a22967f8959a0632f9666a1748126882
Jose Antonio <abreualvarez@hotmail.com>
2020-11-10 18:58:17 -0600

    Se agregaron los archivos de los metodos

---
+++ src/newton.h
@@ -0,0 +1,6 @@
+#ifndef NEWTON_H_INCLUDED
+#define NEWTON_H_INCLUDED
+
+// Aqui pongan los prototipos para el codigo del metodo de Newton
+
+#endif
---
+++ src/secante.h
@@ -0,0 +1,7 @@
+#ifndef SECANTE_H_INCLUDED
+#define SECANTE_H_INCLUDED
+
+// Aqui pongan los prototipos para el codigo del metodo de la secante
+
+
+#endif
