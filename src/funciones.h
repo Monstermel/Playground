840418627fc0e633849dcd2f90b79b5072f9fd89
Jose Antonio <abreualvarez@hotmail.com>
2020-11-10 18:52:53 -0600

    Se agregaron las funciones de las ecuaciones

---
+++ src/funciones.h
@@ -0,0 +1,16 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+double f_1(double x);
+double df_1(double x);
+
+double f_2(double x);
+double df_2(double x);
+
+double f_3(double x);
+double df_3(double x);
+
+double f_4(double x);
+double df_4(double x);
+
+#endif
